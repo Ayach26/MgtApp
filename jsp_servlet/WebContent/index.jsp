e8094ba32cf079957810817aec559fd0aa0b6c7f
Ayaka Fukuda <f26ayach@gmail.com>
2020-12-11 09:37:18 +0900

    サンプルログイン機能追加

---
+++ jsp_servlet/WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html;charset=utf-8" %>
+
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <%String mongon = "IDとパスワードを入力してください。";%>
+  <%= mongon %>
+  <br>
+  <br>
+
+  <form action="/jsp_servlet/Login" method="POST">
+    ID: <input type="text" name="id">
+    パスワード: <input type="text" name="pass">
+    <br>
+    <br>
+    <input type="submit" value="submit">
+  </form>
+</body>
+</html>
---
+++ jsp_servlet/src/info/test/lesson/jsp_servlet/LoginServlet.java
@@ -0,0 +1,63 @@
+package info.test.lesson.jsp_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    // 変数の初期設定
+    String name = "ゲスト"; // 名前の初期値設定
+    String status = "ログイン失敗"; // ログインステータス
+
+    // 文字コードの初期設定
+    request.setCharacterEncoding("utf-8");
+
+    // IDの取得
+    String id = request.getParameter("id");
+
+    // パスワードの取得
+    String pass = request.getParameter("pass");
+
+    // IDとパスワードのチェック
+    if (id.equals("sample") && pass.equals("sample")) {
+      status = "ログイン成功";
+      name = "サンプル";
+    }
+
+    // サーブレット自身で表示する
+    response.setContentType("text/html; charset=utf-8");
+    PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title></title>");
+    out.println("</head>");
+    out.println("<body><br><br>");
+    out.println(status + "<br><br>こんにちは " + name + " さん");
+    out.println("</body>");
+    out.println("</html>");
+
+    // リクエストにデータを追加する
+    // request.setAttribute("login", status);
+    // request.setAttribute("name", name);
+
+    // HttpSessionの作成とセッションにデータを追加する
+    // HttpSession session = request.getSession( true );
+    // session.setAttribute( "name", name );
+
+    // result.jspへ転送
+    // request.getRequestDispatcher("/result.jsp").forward(request, response);
+
+  }
+}
